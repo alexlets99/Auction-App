47177da709c8fbc053e40c757b02fcdde696c7b4
tomoynithsmanassou <alex.letsios@yahoo.gr>
2021-05-30 20:11:50 +0300

    Updates

---
+++ precache-manifest.a6e3d31507db2017ae2e493b8c67b06c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f7581db19525f1ed9d13677fbc0f81",
+    "url": "/Auction-App/index.html"
+  },
+  {
+    "revision": "787fc46ae25e54385726",
+    "url": "/Auction-App/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bde2a8ba4bab09858aa2",
+    "url": "/Auction-App/static/js/2.f387eeae.chunk.js"
+  },
+  {
+    "revision": "787fc46ae25e54385726",
+    "url": "/Auction-App/static/js/main.fe0f0d1a.chunk.js"
+  },
+  {
+    "revision": "be4fc3405bc1b9bd5e6d",
+    "url": "/Auction-App/static/js/runtime-main.3fdaa5ca.js"
+  }
+]);
